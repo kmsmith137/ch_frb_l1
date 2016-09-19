fb16b8204f48d18be55ec8bcf88b0238dc41ef73
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2016-09-19 05:40:42 -0500

    minor edits following api changes in ch_frb_io

--- ch-frb-l1.cpp
+++ ch-frb-l1.cpp
@@ -1,6 +1,7 @@
 // A placeholder for the full L1 process.  Currently data goes through the complete network front end,
 // but doesn't get RFI-cleaned or dedispersed.
 
+#include <cassert>
 #include <errno.h>
 #include <string.h>
 #include <pthread.h>
@@ -11,6 +12,12 @@ using namespace std;
 
 static void *consumer_thread_main(void *opaque_arg)
 {
+    const int nupfreq = 16;
+    const int nalloc = ch_frb_io::constants::nfreq_coarse * nupfreq * ch_frb_io::constants::nt_per_assembled_chunk;
+
+    std::vector<float> intensity(nalloc, 0.0);
+    std::vector<float> weights(nalloc, 0.0);
+
     shared_ptr<ch_frb_io::intensity_beam_assembler> *context = reinterpret_cast<shared_ptr<ch_frb_io::intensity_beam_assembler> *> (opaque_arg);
     shared_ptr<ch_frb_io::intensity_beam_assembler> assembler = *context;
     delete context;
@@ -20,6 +27,9 @@ static void *consumer_thread_main(void *opaque_arg)
 	bool alive = assembler->get_assembled_chunk(chunk);
 	if (!alive)
 	    break;
+
+	assert(chunk->nupfreq == nupfreq);
+	chunk->decode(&intensity[0], &weights[0], ch_frb_io::constants::nt_per_assembled_chunk);
     }
 
     return NULL;
@@ -46,13 +56,13 @@ int main(int argc, char **argv)
     pthread_t consumer_threads[nbeams];
 
     for (int ibeam = 0; ibeam < nbeams; ibeam++) {
-	assemblers[ibeam] = ch_frb_io::intensity_beam_assembler::make(ibeam, false);   // drops_allowed = false
+	assemblers[ibeam] = make_shared<ch_frb_io::intensity_beam_assembler> (ibeam, false);   // drops_allowed = false
 	spawn_consumer_thread(consumer_threads[ibeam], assemblers[ibeam]);
     }
 
     shared_ptr<ch_frb_io::intensity_network_stream> stream = ch_frb_io::intensity_network_stream::make(assemblers, ch_frb_io::constants::default_udp_port);
     stream->start_stream();
-    stream->join_all_threads();
+    stream->join_network_thread();
 
     return 0;
 }
