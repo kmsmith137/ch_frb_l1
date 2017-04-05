6c84a71bc724031f46aca5f2ebbedab305d9de0b
Dustin Lang <dstndstn@gmail.com>
2017-04-05 10:46:37 -0400

    convert t0 to fpgacounts

--- l1b-recv.py
+++ l1b-recv.py
@@ -3,9 +3,11 @@ import zmq
 import msgpack
 import numpy as np
 
+chime_seconds_per_fpga_count = 2.56e-6
+
 class BonsaiCoarseTrigger(object):
     def __init__(self, msgpacked):
-        m = msgpacked[:12]
+        m = msgpacked
         self.version = m[0]
         assert(self.version == 1)
         m = m[1:]
@@ -14,6 +16,7 @@ class BonsaiCoarseTrigger(object):
          self.ndm_fine, self.nt_coarse_per_chunk, self.nsm,
          self.nbeta, self.tm_stride_dm, self.tm_stride_sm, self.tm_stride_beta,
          self.ntr_tot) = m[:15]
+        self.fpga_0 = self.t0 / chime_seconds_per_fpga_count
         m = m[15:]
         trigger_vec = m[0]
         # if packed as binary...
@@ -36,10 +39,11 @@ if __name__ == '__main__':
         msg = msgpack.unpackb(p)
         print('Message:', len(msg))
         for m in msg:
-            print('  type', type(m), len(m))
-            
+            #print('  type', type(m), len(m))
             t = BonsaiCoarseTrigger(m)
             print('  trigger', t)
+            print('  t0', t.t0)
+            print('  fpgacounts:', t.fpga_0)
             print('  ', t.trigger.shape)
             print('  val', t.trigger.ravel()[0])
 
