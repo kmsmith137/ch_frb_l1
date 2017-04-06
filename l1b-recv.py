d637830d6a25b1e3f997d8156309e966d31118db
Dustin Lang <dstndstn@gmail.com>
2017-04-06 12:31:18 -0400

    l1b-recv: listen on all addresses

--- l1b-recv.py
+++ l1b-recv.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
     socket.subscribe('')
-    addr = 'tcp://127.0.0.1:6666'
+    addr = 'tcp://*:6666'
     socket.bind(addr)
 
     i = 0
