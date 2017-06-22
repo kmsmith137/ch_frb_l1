5e955e7ddddbeb04b55e3337b8a905e8d15b8f19
Kendrick Smith <kmsmith@perimeterinstitute.ca>
2017-06-22 17:28:17 -0400

    yaml_paramfile update

--- ch_frb_l1.hpp
+++ ch_frb_l1.hpp
@@ -81,51 +81,86 @@ inline std::vector<T> vconcat(const std::vector<T> &v1, const std::vector<T> &v2
 struct yaml_paramfile {
     const std::string filename;
     YAML::Node yaml;
+    int verbosity;
 
     std::unordered_set<std::string> all_keys;
     mutable std::unordered_set<std::string> requested_keys;
 
-    yaml_paramfile(const std::string &filename);
+    // The 'verbosity' constructor argument has the following meaning:
+    //   0 = quiet
+    //   1 = announce default values for all unspecified parameters
+    //   2 = announce all parameters
+    yaml_paramfile(const std::string &filename, int verbosity=0);
 
     bool has_param(const std::string &k) const;
     void check_for_unused_params(bool fatal=true) const;
 
-    // By default, throws an exception
-    virtual void _die(const std::string &txt) const;
-
-    // For debugging
-    template<typename T> static inline std::string type_name();
+    // For debugging and message-printing
+    virtual void _die(const std::string &txt) const;    // by default, throws an exception
+    virtual void _print(const std::string &txt) const;  // by default, prints to cout
     
+    template<typename T> static inline std::string type_name();
+    template<typename T> static inline std::string stringify(const T &x);
+    template<typename T> static inline std::string stringify(const std::vector<T> &x);
+
+
+    // _read_scalar1(): helper for read_scalar(), assumes key exists
     template<typename T>
-    T read_scalar(const std::string &k) const
+    T _read_scalar1(const std::string &k) const
     {
-	requested_keys.insert(k);
-
-	if (!has_param(k))
-	    _die("parameter '" + k + "' not found");
-
 	try {
 	    return yaml[k].as<T> ();
 	}
 	catch (...) { }
 
 	_die(std::string("expected '") + k + std::string("' to have type ") + type_name<T>());
-	return 0;   // compiler pacifier
+	throw std::runtime_error("yaml_paramfile::_die() returned?!");
     }
 
-    // Automatically converts a scalar to length-1 vector.
+    // _read_scalar2(): helper for read_scalar(), assumes key exists
     template<typename T>
-    std::vector<T> read_vector(const std::string &k) const
+    T _read_scalar2(const std::string &k) const
     {
+	T ret = _read_scalar1<T> (k);
 	requested_keys.insert(k);
 
+	if (verbosity >= 2)
+	    _print(k + " = " + stringify(ret) + "\n");
+
+	return ret;
+    }
+
+    // "Vanilla" version of read_scalar()
+    template<typename T>
+    T read_scalar(const std::string &k) const
+    {
 	if (!has_param(k))
 	    _die("parameter '" + k + "' not found");
 
+	return _read_scalar2<T> (k);
+    }
+
+    // This version of read_scalar() has a default value, which is returned if the key is not found.
+    template<typename T>
+    T read_scalar(const std::string &k, T default_val) const
+    {
+	if (!has_param(k)) {
+	    if (verbosity >= 1)
+		_print("parameter '" + k + "' not found, using default value " + stringify(default_val) + "\n");
+	    return default_val;
+	}
+
+	return _read_scalar2<T> (k);
+    }
+
+
+    // _read_vector1(): helper for read_vector(), assumes key exists
+    // Automatically converts a scalar to length-1 vector.
+    template<typename T>
+    std::vector<T> _read_vector1(const std::string &k) const
+    {
 	try {
-	    std::vector<T> ret = yaml[k].as<std::vector<T>> ();
-	    if (ret.size() > 0)
-		return ret;
+	    return yaml[k].as<std::vector<T>> ();
 	}
 	catch (...) { }
 
@@ -135,14 +170,70 @@ struct yaml_paramfile {
 	catch (...) { }
 
 	_die("expected '" + k + "' to have type " + type_name<T>() + ", or be a list of " + type_name<T>() + "s");
-	return { };   // compiler pacifier
+	throw std::runtime_error("yaml_paramfile::_die() returned?!");	
+    }
+
+    // _read_vector2(): helper for read_vector(), assumes key exists
+    template<typename T>
+    std::vector<T> _read_vector2(const std::string &k) const
+    {
+	std::vector<T> ret = _read_vector1<T> (k);
+	requested_keys.insert(k);
+
+	if (verbosity >= 2)
+	    _print(k + " = " + stringify(ret) + "\n");
+
+	return ret;
+    }
+
+    // "Vanilla" version of read_vector().
+    // Automatically converts a scalar to length-1 vector.
+    template<typename T>
+    std::vector<T> read_vector(const std::string &k) const
+    {
+	if (!has_param(k))
+	    _die("parameter '" + k + "' not found");
+
+	return _read_vector2<T> (k);
+    }
+
+    // This version of read_vector() has a default value, which is returned if the key is not found.
+    template<typename T>
+    std::vector<T> read_vector(const std::string &k, const std::vector<T> default_val) const
+    {
+	if (!has_param(k)) {
+	    if (verbosity >= 1)
+		_print("parameter '" + k + "' not found, using default value " + stringify(default_val) + "\n");
+	    return default_val;
+	}
+
+	return _read_vector2<T> (k);
     }
 };
 
 
 template<> inline std::string yaml_paramfile::type_name<int> () { return "int"; }
+template<> inline std::string yaml_paramfile::type_name<float> () { return "float"; }
 template<> inline std::string yaml_paramfile::type_name<double> () { return "double"; }
 template<> inline std::string yaml_paramfile::type_name<std::string> () { return "string"; }
+
+
+template<typename T> inline std::string yaml_paramfile::stringify(const T &x)
+{
+    std::stringstream ss;
+    ss << x;
+    return ss.str();
+}
+
+template<typename T> inline std::string yaml_paramfile::stringify(const std::vector<T> &x)
+{
+    std::stringstream ss;
+    ss << "[ ";
+    for (size_t i = 0; i < x.size(); i++)
+	ss << (i ? ", " : "") << x[i];
+    ss << " ]";
+    return ss.str();
+}
 
 
 }  // namespace ch_frb_l1
--- yaml_paramfile.cpp
+++ yaml_paramfile.cpp
@@ -13,7 +13,7 @@ namespace ch_frb_l1 {
 #endif
 
 
-static bool file_exists(const string &filename)
+bool file_exists(const string &filename)
 {
     struct stat s;
 
@@ -27,9 +27,12 @@ static bool file_exists(const string &filename)
 }
 
 
-yaml_paramfile::yaml_paramfile(const string &filename_) :
-    filename(filename_)
+yaml_paramfile::yaml_paramfile(const string &filename_, int verbosity_) :
+    filename(filename_), verbosity(verbosity_)
 {
+    if (verbosity < 0 || verbosity > 2)
+	_die("verbosity constructor argument (=" + stringify(verbosity) + ") must be 0, 1, or 2");
+
     if (!file_exists(filename))
 	_die("file not found");
 
@@ -91,6 +94,12 @@ void yaml_paramfile::check_for_unused_params(bool fatal) const
 void yaml_paramfile::_die(const string &txt) const
 {
     throw runtime_error(filename + ": " + txt);
+}
+
+// Virtual member function; this is the default implementation
+void yaml_paramfile::_print(const string &txt) const
+{
+    cout << filename << ": " << txt << flush;
 }
 
 
