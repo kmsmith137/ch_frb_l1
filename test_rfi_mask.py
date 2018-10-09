c29d68b98bc77787fd07cd1afcafcef03076418e
Dustin Lang <dstndstn@gmail.com>
2018-10-09 12:12:19 -0400

    test-rfi-mask: exec L1 process; call prometheus metrics

--- test_rfi_mask.py
+++ test_rfi_mask.py
@@ -3,90 +3,84 @@ import simulate_l0
 import numpy as np
 from rpc_client import read_msgpack_file, RpcClient
 from time import sleep
+import subprocess
+import os
+from glob import glob
+
+print('Deleting existing msgpack files...')
+fns = glob('chunk-test-rfi-mask-*.msgpack')
+for fn in fns:
+    print('  ', fn)
+    os.remove(fn)
 
 l0 = simulate_l0.l0sim('l0_configs/l0_rfi.yml', 1.0)
 client = RpcClient({'a':'tcp://127.0.0.1:5555'})
 
-if True:
-    beam_id = 0
-    fpga_counts_per_sample = 384
-    nt = 1024
-    nf = 16384
-    nrfi = 1024
-    nt_coarse = nt // 16
-    nf_coarse = 1024
+l1cmd = './ch-frb-l1 -fv l1_configs/l1_dstn.yaml rfi_configs/rfi_testing.json bonsai_production_noups_nbeta1_v2.hdf5 xxx'
+l1 = subprocess.Popen(l1cmd, shell=True)
+
+sleep(5)
+
+beam_id = 0
+fpga_counts_per_sample = 384
+nt = 1024
+nf = 16384
+nrfi = 1024
+nt_coarse = nt // 16
+nf_coarse = 1024
+
+chunk0 = 50
+
+offset = np.empty((nf_coarse, nt_coarse), np.float32)
+scale = np.empty((nf_coarse, nt_coarse), np.float32)
+rfi = None
+
+offset[:,:] = -128.
+scale[:,:] = 1.
+
+prom_cmd = 'wget http://127.0.0.1:9999/metrics -O -'
+
+writereq = None
+
+for i in range(20):
+    data = np.clip(128. + 20. * np.random.normal(size=(nf, nt)), 1, 254).astype(np.uint8)
+    # Make an RFI spike
+    data[:, i*50:i*50+100] = 200
+    ichunk = i + chunk0
     
-    offset = np.empty((nf_coarse, nt_coarse), np.float32)
-    scale = np.empty((nf_coarse, nt_coarse), np.float32)
-    rfi = None
+    ch = simulate_l0.assembled_chunk(beam_id, fpga_counts_per_sample, ichunk,
+                                     data, offset, scale, rfi)
+    print('Chunk:', ch)
     
-    offset[:,:] = -128.
-    scale[:,:] = 1.
-    
-    for i in range(10):
-        data = np.clip(128. + 20. * np.random.normal(size=(nf, nt)), 1, 254).astype(np.uint8)
-        data[:, i*50:i*50+100] = 200
-        ichunk = i + 50
+    print('Sending chunk...')
+    l0.send_chunk(0, ch)
+
+
+    if i == 5:
+        os.system(prom_cmd)
+
+        print('After sending 5 chunks:')
+        print(client.get_statistics())
         
-        ch = simulate_l0.assembled_chunk(beam_id, fpga_counts_per_sample, ichunk,
-                                         data, offset, scale, rfi)
-        #ch.write('chunk-%02i.msgpack' % i)
+        print('Sending write request...')
+        res = client.write_chunks([0], 0, (50 + 4) * 384 * 1024,
+                                  'chunk-test-rfi-mask-(FPGA0).msgpack', need_rfi=True,
+                                  waitAll=False)
+        print('Got write request result:', res)
+        reqs,token = res
+        writereq = reqs[0]
+        for c in writereq:
+            print('  ', c)
+        sleep(1)
 
-        print('Sending chunk...')
-        l0.send_chunk(0, ch)
-        
-    #ch2 = read_msgpack_file('chunk.msgpack')
 
-    import sys
-    sys.exit(0)
-    
+    if writereq is not None:
+        for c in writereq:
+            print('Status of', c.filename, ':', client.get_writechunk_status(c.filename))
 
-l0 = simulate_l0.l0sim('l0_configs/l0_rfi.yml', 1.0)
+    # Give L1 some time to process...
+    sleep(2)
 
-#l0.send_chunk_file(0, 'chunk-0000-000000000000.msgpack')
+os.system(prom_cmd)
 
-client = RpcClient({'a':'tcp://127.0.0.1:5555'})
-
-for i in range(5):
-    l0.send_chunk_file(0, 'chunk-%02i.msgpack' % i)
-
-print()
-sleep(3)
-
-print('After sending 5 chunks:')
-print(client.get_statistics())
-
-print('Sending write request...')
-res = client.write_chunks([0], 0, (50 + 4) * 384 * 1024,
-                          'chunks-out-(FPGA0).msgpack', need_rfi=True,
-                          waitAll=False)
-print('Got write request result:', res)
-reqs,token = res
-req = reqs[0]
-for c in req:
-    print('  ', c)
-sleep(1)
-
-for c in req:
-    print('Status of', c.filename, ':', client.get_writechunk_status(c.filename))
-
-sleep(1)
-
-print('Sending more data...')
-for i in range(5, 10):
-    l0.send_chunk_file(0, 'chunk-%02i.msgpack' % i)
-
-    for c in req:
-        s = client.get_writechunk_status(c.filename)
-        print('Status of', c.filename, ':', s[0])
-    
-for i in range(10):
-    sleep(1)
-    print()
-    for c in req:
-        s = client.get_writechunk_status(c.filename)
-        print('Status of', c.filename, ':', s[0])
-    
-
-#print('RPC client received tokens:', client.received)
-
+l1.terminate()
