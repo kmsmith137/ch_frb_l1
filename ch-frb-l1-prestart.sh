b9a40896546a4eb5124851d3f2a9059358c7e091
l1operator <chitrang.patel@yahoo.com>
2020-02-14 12:27:09 -0800

    add pre/post start/stop scripts

---
+++ ch-frb-l1-prestart.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+#echo "ch-frb-l1-prestart.sh script.  I am $(whoami)"
+
+### Instead, just run this in ch-frb-l1-dispatch.sh
+#if [ $(hostname) == cf5n3 ]; then
+#    sudo /home/l1operator/start-singlebeam.sh
+#fi
